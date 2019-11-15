a82a29a4c327a79598cd0d45fdab4d63dca139d6
9codes <57799072+9codes@users.noreply.github.com>
2019-11-15 18:20:34 +0530

    Add files via upload

---
+++ pbe12.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 14 06:35:10 2019
+
+@author: root
+"""
+
+print("***Wellcome***")
+print("***You are going to add first integer to second integer!!!***")
+print("***And you must type an integer in the input box to get valid result.***")
+number1 = input("Please type first integer:")
+number2 = input("Please type second integer:")
+number1 = int(number1)
+number2 = int(number2)
+print(number1,"+",number2,"=",number1+number2)
