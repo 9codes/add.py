dd4a9a0f5fdee71afa8bb13e180a1991e7c3835a
9codes <57799072+9codes@users.noreply.github.com>
2019-11-15 18:24:58 +0530

    Update and rename pbe12.py to add.py

--- add.py
+++ add.py
@@ -1,6 +1,5 @@
 #Created on Thu Nov 14 06:35:10 2019
-#author: shaqlin(9codes)
-
+#author: Shaqlin Mustak(9codes)
 print("***Wellcome***")
 print("***You are going to add first integer to second integer!!!***")
 print("***And you must type an integer in the input box to get valid result.***")
